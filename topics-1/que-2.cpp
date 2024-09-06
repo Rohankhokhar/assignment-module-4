03ebd48484545701b043f9d7d4b5e4a9e09a2362
Rohankhokhar <167191892+Rohankhokhar@users.noreply.github.com>
2024-09-06 16:45:30 +0530

    Add files via upload

---
+++ topics-1/que-2.cpp
@@ -0,0 +1,33 @@
+//What is OOP? List OOP concepts :-
+
+oop :-
+Object-Oriented Programming (OOP) is a programming paradigm centered around the concept of objects, which can contain data and code. The data is stored in fields (also known as attributes or properties), and the code is stored in methods (also known as functions or procedures). OOP is designed to improve software modularity and reusability by organizing software design around objects rather than functions or logic.
+
+Here are the key concepts of OOP:
+
+1)Class: A blueprint or template for creating objects. It defines a set of attributes and methods that the created objects will have.
+
+2)Object: An instance of a class. It contains data and methods that operate on the data. In OOP, everything is considered an object.
+
+3)Encapsulation: The bundling of data and methods that operate on that data within a single unit (class). It restricts direct access to some of the object's components, which can prevent the accidental modification of data.
+
+4)Abstraction: The concept of hiding the complex implementation details and showing only the essential features of the object. It helps to reduce complexity and increase efficiency.
+
+5)Inheritance: The mechanism by which one class (called a subclass or derived class) can inherit properties and methods from another class (called a superclass or base class). It allows for code reuse and the creation of a hierarchical relationship between classes.
+
+6)Polymorphism: The ability of objects to take on multiple forms. It allows methods to do different things based on the object it is acting upon, even though they share the same interface. There are two types of polymorphism: compile-time (method overloading) and run-time (method overriding).
+
+1] class and object
+syntax of class:
+class ClassName{
+    // visibility :
+    // data member [attribute and property]
+    // member function [method and function]
+}
+
+ClassName obj_name; // syntax of object
+
+2] encapsulation
+3] inheritance
+4] abstraction
+5] polymorphisum
---
+++ topics-1/que-3.cpp
@@ -0,0 +1,30 @@
+//What is the difference between OOP and POP? :-
+Object-Oriented Programming (OOP) and Procedure-Oriented Programming (POP) are two different programming paradigms that have distinct approaches to software design and development. Here are the key differences between OOP and POP:
+
+1. Basic Concept :-
+OOP: Centers around the concept of objects, which are instances of classes. These objects encapsulate data and behavior, making it easier to model real-world entities.
+POP: Focuses on the sequence of tasks or procedures to be performed. It structures the program as a series of functions or procedures.
+
+2. Data and Functions :-
+OOP: Data and functions are bundled together in objects. Data is accessed and manipulated through methods, maintaining encapsulation.
+POP: Data and functions are separate. Functions operate on data, which is often passed around in the form of arguments.
+
+3. Modularity and Reusability :-
+OOP: Encourages modularity through classes and objects. Inheritance, polymorphism, and encapsulation facilitate code reuse and flexibility.
+POP: Modularity is achieved by organizing the program into functions. However, reusability is often limited compared to OOP because of the lack of support for inheritance and polymorphism.
+
+4. Data Security:-
+OOP: Data is encapsulated within objects, providing a mechanism for data protection. Access to data can be controlled using access modifiers (public, private, protected).
+POP: Data is usually global and accessible by any function, which can lead to security issues and unintentional data modification.
+
+5. Approach to Problem Solving :-
+OOP: Focuses on modeling entities and their interactions. The design revolves around identifying the objects and their relationships.
+POP: Focuses on breaking down the problem into a series of steps or functions. The design revolves around the sequence of actions to be performed.
+
+6. Scalability and Maintenance :-
+OOP: More scalable and easier to maintain because of its modular nature. It is easier to update and manage large codebases.
+POP: Less scalable and more difficult to maintain, especially for large projects. Changes in data structures or procedures can require extensive modifications.
+
+7. Examples of Languages :-
+OOP: Languages like Java, C++, Python (supports both OOP and POP), and C#.
+POP: Languages like C, Pascal, and early versions of BASIC.
